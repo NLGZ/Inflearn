bafb435f23a47e213f61dd1348618d7dc349f9cd
NLGZ <edas96@naver.com>
2024-01-23 20:24:44 +0900

    0123 자습

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +21,31 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+    @GetMapping("hello-string")
+    @ResponseBody // 응답 body부에 값을 직접 내려보냄
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+
+        }
+    }
+
+
 }
